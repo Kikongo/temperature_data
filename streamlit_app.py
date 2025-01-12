75db6264067053197e3aee5c8d13f72c7168a438
Kikongo <tastingtaste63@gmail.com>
2025-01-12 22:24:29 +0900

    final app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
-import math
 from pathlib import Path
+import json
+import httpx
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
+    page_title='Temperature data',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
@@ -13,8 +14,8 @@ st.set_page_config(
 # Declare some useful functions.
 
 @st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+def get_temp_data():
+    """Grab temperature data from a CSV file.
 
     This uses caching to avoid having to read the file every time. If we were
     reading from an HTTP endpoint instead of a file, it's a good idea to set
@@ -22,61 +23,27 @@ def get_gdp_data():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
+    DATA_FILENAME = Path(__file__).parent/'data/temperature_data_processed.csv'
     raw_gdp_df = pd.read_csv(DATA_FILENAME)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    MIN_YEAR = 2010
+    MAX_YEAR = 2019
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+    raw_gdp_df['timestamp'] = pd.to_datetime(raw_gdp_df['timestamp'])
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+    return raw_gdp_df
 
-    return gdp_df
+temp_df = get_temp_data()
 
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
+# :earth_americas: Temperature data
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+Исследуй исторические температурные данные.
 '''
+st.dataframe(temp_df.head(5))
 
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+min_value = temp_df['timestamp'].dt.year.min()
+max_value = temp_df['timestamp'].dt.year.max()
 
 from_year, to_year = st.slider(
     'Which years are you interested in?',
@@ -84,68 +51,88 @@ from_year, to_year = st.slider(
     max_value=max_value,
     value=[min_value, max_value])
 
-countries = gdp_df['Country Code'].unique()
+cities = temp_df['city'].unique()
 
-if not len(countries):
-    st.warning("Select at least one country")
+if not len(cities):
+    st.warning("Select at least one city")
 
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
+selected_cities = st.multiselect(
+    'Which cities would you like to view?',
+    cities,
+    ['New York'])
 
 # Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
+filtered_gdp_df = temp_df[
+    (temp_df['city'].isin(selected_cities))
+    & (temp_df['timestamp'].dt.year <= to_year)
+    & (from_year <= temp_df['timestamp'].dt.year)
 ]
 
-st.header('GDP over time', divider='gray')
-
-''
+st.header('Temperature over time', divider='gray')
 
 st.line_chart(
     filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+    x='timestamp',
+    y='temperature',
+    color='city',
 )
 
-''
-''
+if st.checkbox("Показать среднюю температуру по сезонам"):
+    st.line_chart(
+        filtered_gdp_df,
+        x='timestamp',
+        y='seasonal_mean',
+        color='city',
+    )
+
+if st.checkbox("Показать среднее стандартное отклонение"):
+    st.line_chart(
+        filtered_gdp_df,
+        x='timestamp',
+        y='seasonal_std',
+        color='city',
+    )
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+API_TOKEN = st.text_input("Введите API  API-ключа OpenWeatherMap")
 
-st.header(f'GDP in {to_year}', divider='gray')
+if len(API_TOKEN) > 0:
+    try:
+        URL_temp = "https://api.openweathermap.org/data/2.5/weather"
+        URL_geocode = "http://api.openweathermap.org/geo/1.0/direct"
+        season = "winter"
 
-''
+        for city in selected_cities:
+            new_df = filtered_gdp_df[(filtered_gdp_df['city'] == city) & (filtered_gdp_df['season'] == season)].iloc[0]
+            data_geo = {
+                "q": city,
+                "appid": API_TOKEN
+            }
 
-cols = st.columns(4)
+            r_geocode = httpx.get(URL_geocode, params=data_geo)
+            resp_geo = json.loads(r_geocode.text)
+            lat = resp_geo[0]['lat']
+            lon = resp_geo[0]['lon']
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+            data_temp = {
+                "lat": lat,
+                "lon": lon,
+                "appid": API_TOKEN
+            }
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+            r_temp = httpx.get(URL_temp, params=data_temp)
+            resp_temp = json.loads(r_temp.text)
+            temp = resp_temp['main']['temp']
+            temp_celsius = temp - 273.15
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+            anomaly = (temp < (new_df['seasonal_mean'] - 2 * new_df['seasonal_std'])) | \
+                                (new_df['temperature'] > (new_df['seasonal_mean'] + 2 * new_df['seasonal_std']))
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+            st.subheader(city)
+            st.write("Температура: ", round(temp_celsius, 2))
+            if anomaly == True:
+                st.write("Это аномальная температура")
+            else:
+                st.write("Это нормальная температура")
+    except Exception as e:
+        st.error("cod:401, message: Invalid API key. Please see https://openweathermap.org/faq#error401 for more info.")
